15563c51954e266945acbd7883a9ac79d0cc6ee2
Stephane Bellity <sbellity@gmail.com>
2014-06-10 20:24:17 +0200

    Extract hashtags from image descriptions

--- app/widgets/new_picture/main.js
+++ app/widgets/new_picture/main.js
@@ -8,16 +8,25 @@ Hull.widget('new_picture', {
 
   actions: {
     send: function (event, options) {
+      var _ = this.sandbox.util._;
       $(options.el).addClass('disabled-state');
       var textarea = document.getElementById('picture-description');
 
       this.sandbox.emit('hullagram.savingPicture');
 
-      this.api.post('/me/images', {
-        description: textarea.value,
+      var description = textarea.value;
+      var tags = _.map(description.match(/\B#([^ ]+)/g), function(t) {
+        return t.replace('#', '');
+      });
+
+      var img = {
+        description: description,
         source_url: this.options.source_url,
-        name: this.options.name
-      }).then(function (image) {
+        name: this.options.name,
+        tags: tags
+      };
+
+      this.api.post('/me/images', img).then(function (image) {
         this.sandbox.emit('hullagram.pictureSaved', image);
       }.bind(this));
     }
